164aba4a36bbcda62848b5160fc44ca3c84f65a3
theRVR3ND <kelvinwpeng@gmail.com>
2016-03-03 15:24:42 -0500

    Conception of the file

---
+++ BattleshipAI.java
@@ -0,0 +1,8 @@
+import java.awt.*;
+
+public class BattleshipAI{
+   public static void main(String[] args){
+      JFrame frame = new JFrame("The Glorious Battleship AI?");
+      frame.setSize();
+   }
+}
